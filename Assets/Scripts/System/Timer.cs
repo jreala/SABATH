1859581c152bb8e6c514912040160fc2845959db
Jason <jeala510@gmail.com>
2017-11-11 18:45:25 -0800

    Timer implemented. TG-2 #done

---
+++ Assets/Scripts/System/Timer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timer;
+    public float maxTime = 2;
+    public bool ended = false;
+
+	void Update () {
+        if (ended)
+        {
+            return;
+        }
+
+        var minutes = (int)(Time.timeSinceLevelLoad / 60f);
+        var seconds = (int)(Time.timeSinceLevelLoad % 60f);
+        var currentMinutes = Mathf.Clamp(maxTime - minutes, 0, 3);
+        var currentSeconds = Mathf.Clamp(59 - seconds, 0, 59);
+
+        if(currentMinutes == 0 && currentSeconds == 0)
+        {
+            OnFinish();
+        }
+
+        timer.text = currentMinutes.ToString("0") + ":" + currentSeconds.ToString("00");
+    }
+
+    public virtual void OnFinish()
+    {
+        ended = true;
+        Debug.Log("End.");
+    }
+}
